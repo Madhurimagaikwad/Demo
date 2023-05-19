d582e05edff895a5c8ccef34672f8fc9ab4e2013
AnushkaReva <revati.kt@gamil.com>
2023-05-19 16:27:48 +0530

    Product class getter setter

--- src/main/java/com/velocity/model/Product.java
+++ src/main/java/com/velocity/model/Product.java
@@ -1,12 +1,67 @@
 package com.velocity.model;
 
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 import javax.persistence.Table;
 
 @Entity
 @Table(name="produce")
 public class Product {
 	
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
 	private int id;
+	private String Productname;
+	private String Manufactureddate;
+	private String expirydate;
+	private int quantity;
+	private double price;
+	private int Productcode;
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getProductname() {
+		return Productname;
+	}
+	public void setProductname(String productname) {
+		Productname = productname;
+	}
+	public String getManufactureddate() {
+		return Manufactureddate;
+	}
+	public void setManufactureddate(String manufactureddate) {
+		Manufactureddate = manufactureddate;
+	}
+	public String getExpirydate() {
+		return expirydate;
+	}
+	public void setExpirydate(String expirydate) {
+		this.expirydate = expirydate;
+	}
+	public int getQuantity() {
+		return quantity;
+	}
+	public void setQuantity(int quantity) {
+		this.quantity = quantity;
+	}
+	public double getPrice() {
+		return price;
+	}
+	public void setPrice(double price) {
+		this.price = price;
+	}
+	public int getProductcode() {
+		return Productcode;
+	}
+	public void setProductcode(int productcode) {
+		Productcode = productcode;
+	}
+	
+	
 
 }
