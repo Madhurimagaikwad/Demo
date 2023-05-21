e9a3df84c844022807f4212c96e4a4e106043229
AnushkaReva <revati.kt@gamil.com>
2023-05-21 15:18:08 +0530

    User Login

--- src/main/java/com/velocity/repository/UserLoginRepository.java
+++ src/main/java/com/velocity/repository/UserLoginRepository.java
@@ -1,7 +1,16 @@
-/*package com.velocity.repository;
+package com.velocity.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
 
-public interface UserLoginRepository extends JpaRepository<UserLoginRepository, Integer> {
+import com.velocity.model.UserLogin;
 
-}*/
+@Repository
+public interface UserLoginRepository extends JpaRepository<UserLogin, Integer> {
+
+	// How to write native query / SQL query
+
+	@Query(value = "select * from userlogin e where password=?1 ", nativeQuery = true)
+	public UserLogin getUserLoginByPassword(String password);
+}
--- src/main/java/com/velocity/service/UserLoginService.java
+++ src/main/java/com/velocity/service/UserLoginService.java
@@ -1,5 +1,9 @@
 package com.velocity.service;
 
+import com.velocity.model.UserLogin;
+
 public interface UserLoginService {
+	
+	public UserLogin getUserLoginByPassword(String password);
 
 }
